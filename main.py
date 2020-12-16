2328ce3fbfb8fd4c0cadfe1c8aee87453ddaef63
miha <mihanvr@gmail.com>
2020-12-16 19:50:55 +0300

    Д.З.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fio.path import Path
 root_path = Path("test_dir")
 print(root_path.exists())
 print(root_path.get_items())
+# Написать тесты для всех функций в Path'е
 
 
 # assert Directory("test_dir").exists()
