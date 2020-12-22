cf87ce7bdef04983f839e125021e27b67eb668f6
VavakinV <vladvavakin@gmail.com>
2020-12-22 18:14:56 +0300

    Добавлены основные функции в класс "Path"

--- fio/path.py
+++ fio/path.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import os
 
 class Path:
 
@@ -9,20 +9,31 @@ class Path:
     def path(self):
         return self.path
 
-    def exists(self):
-        pass
+    def exists(self) -> bool:
+        return os.path.isfile(self.path) or os.path.isdir(self.path)
 
     def get_items(self) -> List['Path']:
-        pass
+        if os.path.isdir(self.path):
+            items_list = []
+            for element in os.listdir(self.path):
+                f_path = self.path + '/' + element
+                items_list.append(Path(f_path))
+            return items_list
+        else:
+            raise KeyError("Путь не существует или не является директорией")
 
     def is_file(self):
-        pass
+        return os.path.isfile(self.path)
 
     def is_dir(self):
-        pass
+        return os.path.isdir(self.path)
 
     def get_content(self):
-        pass
+        if os.path.isfile(self.path):
+            with open(self.path, 'r') as file:
+                return str(file)
+        else:
+            raise KeyError("Путь не существует или не является файлом")
 
     def __str__(self):
         return self.path
